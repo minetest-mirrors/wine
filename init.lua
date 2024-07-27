69f2827805689d58c6bae3cd55a05f897a92e3e9
tenplus1 <kinsellaja@yahoo.com>
2024-07-27 18:29:06 +0100

    update {eatable} function

--- init.lua
+++ init.lua
@@ -70,8 +70,7 @@ function wine:add_item(list)
 end
 
 -- helper function
-
-local mod_tt_base = minetest.get_modpath("tt_base")
+local mod_tt_base = minetest.get_modpath("tt_base") -- mod does similar to infotext
 
 function wine.add_eatable(item, hp)
 
@@ -79,17 +78,17 @@ function wine.add_eatable(item, hp)
 
 	if def then
 
-		local grps = def.groups or {}
+		local groups = table.copy(def.groups) or {}
 		local txt = " (" ; if hp > 0 then txt = txt .. "+" end
 		txt = txt .. hp .. " HP)"
 
-		grps.eatable = hp ; grps.flammable = 2
+		groups.eatable = hp ; groups.flammable = 2
 
 		if mod_tt_base == nil then
 			def.description = def.description .. txt
 		end
 
-		minetest.override_item(item, {description = def.description, groups = grps})
+		minetest.override_item(item, {description = def.description, groups = groups})
 	end
 end
 
